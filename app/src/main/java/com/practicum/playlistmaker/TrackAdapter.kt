3c992be1c55d220e15e7383996b92aa535c6c687
Хартов Андрей Александрович <khartov.a.al@sberbank.ru>
2025-05-20 22:01:28 +0300

    move LayoutInflater to ViewHolder

--- app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
@@ -1,6 +1,5 @@
 package com.practicum.playlistmaker
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
@@ -9,8 +8,7 @@ class TrackAdapter(
 ) : RecyclerView.Adapter<TrackViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
-        return TrackViewHolder(view)
+        return TrackViewHolder(parent)
     }
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
--- app/src/main/java/com/practicum/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackViewHolder.kt
@@ -2,14 +2,17 @@ package com.practicum.playlistmaker
 
 import android.net.Uri
 import android.util.TypedValue
-import android.view.View
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
-class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class TrackViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
+) {
     private val artistName = itemView.findViewById<TextView>(R.id.artist_name)
     private val trackName = itemView.findViewById<TextView>(R.id.track_name)
     private val trackTime = itemView.findViewById<TextView>(R.id.track_time)
@@ -19,7 +22,7 @@ class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         artistName.text = track.artistName
         trackName.text = track.trackName
         trackTime.text = track.trackTime
-        
+
         Glide.with(itemView.context)
             .load(Uri.parse(track.artworkUrl100))
             .placeholder(R.drawable.track_placeholder)
