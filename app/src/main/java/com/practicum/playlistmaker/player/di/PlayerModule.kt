c9c8620b32b9996ec76a5b92309adbf2e124e00e
Хартов Андрей Александрович <khartov.a.al@sberbank.ru>
2025-10-19 22:50:00 +0300

    COROUTINES: обновление позиции трека

--- app/src/main/java/com/practicum/playlistmaker/player/di/PlayerModule.kt
+++ app/src/main/java/com/practicum/playlistmaker/player/di/PlayerModule.kt
@@ -2,7 +2,7 @@ package com.practicum.playlistmaker.player.di
 
 import android.media.MediaPlayer
 import com.practicum.playlistmaker.player.data.impl.TrackDisplayInteractorImpl
-import com.practicum.playlistmaker.player.data.network.MediaPlayerService
+import com.practicum.playlistmaker.player.data.network.AudioPlayerImpl
 import com.practicum.playlistmaker.player.domain.api.AudioPlayer
 import com.practicum.playlistmaker.player.domain.model.TrackDisplayInteractor
 import com.practicum.playlistmaker.player.ui.PlayerViewModel
@@ -13,7 +13,7 @@ import org.koin.dsl.module
 
 val playerModule = module {
     single<TrackDisplayInteractor> { TrackDisplayInteractorImpl(androidContext()) }
-    single<AudioPlayer> { MediaPlayerService(get()) }
+    single<AudioPlayer> { AudioPlayerImpl(get()) }
     single { MediaPlayer() }
 }
 
--- app/src/main/java/com/practicum/playlistmaker/player/ui/PlayerState.kt
+++ app/src/main/java/com/practicum/playlistmaker/player/ui/PlayerState.kt
@@ -1,8 +1,13 @@
 package com.practicum.playlistmaker.player.ui
 
-sealed interface PlayerState {
-    object ReadyToPlay: PlayerState
-    object Play: PlayerState
-    object Pause: PlayerState
-    object Stop: PlayerState
+import com.practicum.playlistmaker.search.domain.model.TrackMapper
+
+sealed class PlayerState(val isPlaying: Boolean, val trackTimePosition: String) {
+    class Playing(position: Int) : PlayerState(true, TrackMapper.millisToMMSS(position))
+    class Paused(position: Int) : PlayerState(false, TrackMapper.millisToMMSS(position))
+    class Stopped : PlayerState(false, TRACK_STOPPED_TEXT)
+
+    companion object {
+        val TRACK_STOPPED_TEXT = "00:00"
+    }
 }
