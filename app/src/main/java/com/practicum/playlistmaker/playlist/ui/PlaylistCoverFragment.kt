cbda7a2f32ac890dca1d191673537034208ab48d
Хартов Андрей Александрович <khartov.a.al@sberbank.ru>
2025-12-17 23:24:50 +0300

    Переход на TextInputLayout + TextInputEditText

--- app/src/main/java/com/practicum/playlistmaker/playlist/ui/PlaylistCoverFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/playlist/ui/PlaylistCoverFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
@@ -45,12 +44,10 @@ abstract class PlaylistCoverFragment : BindingFragment<FragmentPlaylistEditorBin
         binding.playlistTitle.addTextChangedListener { text ->
             getViewModel().playlistName = text.toString()
             binding.newPlaylistButton.isEnabled = getViewModel().canSavePlaylist()
-            binding.playlistTitleHint.isVisible = !text.isNullOrBlank()
         }
 
         binding.playlistDescription.addTextChangedListener { text ->
             getViewModel().playlistDescription = text.toString()
-            binding.playlistDescriptionHint.isVisible = !text.isNullOrBlank()
         }
     }
 
