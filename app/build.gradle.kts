bc7d676d5aabba7c13766e1094a78b0a042f3e9d
Хартов Андрей Александрович <khartov.a.al@sberbank.ru>
2026-01-31 20:36:39 +0300

    Включил Compose и повысил версии

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,11 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.google.kspPlugin)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -32,14 +35,18 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
     }
+
     ksp {
         arg("room.schemaLocation", "$projectDir/schemas")
     }
     buildFeatures {
         viewBinding = true
+        compose = true
     }
 }
 
@@ -65,6 +72,11 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.material3)
+    implementation(libs.coil)
+
     annotationProcessor(libs.room.compiler)
     ksp(libs.room.compiler)
     testImplementation(libs.junit)
